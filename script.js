be015c461963d545dccf308e40dd1b745ea304a3
FRJ74 <frankr74@gmail.com>
2024-01-01 20:51:29 -0600

    Change value of goStore() with innerText

--- script.js
+++ script.js
@@ -27,8 +27,9 @@ button3.onclick = fightDragon;
 // goStore function
 
 function goStore() {
-    console.log("Going to store.");
-  
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";  
 }
 
 // goCave function
