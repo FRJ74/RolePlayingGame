10b6883df0f451fc24da66fcc719ee95445fd2e5
FRJ74 <frankr74@gmail.com>
2024-01-10 17:47:26 -0600

    Removed extra spaces in weapons array

--- script.js
+++ script.js
@@ -20,33 +20,10 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 
 const weapons = [
   
-    {
-  
-      name:"stick",
-  
-      power: 5,
-    },
-    {
-  
-      name: "dagger",
-  
-      power: 30,
-  
-    },
-    {
-  
-      name:"claw hammer",
-  
-      power: 50,
-  
-    },
-    {
-  
-      name:"sword",
-  
-      power: 100,
-  
-    }
+    { name:"stick", power: 5 },
+    { name: "dagger", power: 30 },
+    { name:"claw hammer", power: 50 },
+    { name:"sword", power: 100 }
   ];
 
 const locations = [
