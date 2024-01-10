f1797d76096315cb0d794fdc32ce1c9f2e606405
FRJ74 <frankr74@gmail.com>
2024-01-10 17:39:31 -0600

    Add four object to weapon array

--- script.js
+++ script.js
@@ -17,7 +17,38 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
-const weapons = [];
+
+const weapons = [
+  
+    {
+  
+      name:"stick",
+  
+      power: 5,
+    },
+    {
+  
+      name: "dagger",
+  
+      power: 30,
+  
+    },
+    {
+  
+      name:"claw hammer",
+  
+      power: 50,
+  
+    },
+    {
+  
+      name:"sword",
+  
+      power: 100,
+  
+    }
+  ];
+
 const locations = [
   {
     name: "town square",
@@ -97,7 +128,9 @@ function goCave() {
   }
   
   function buyWeapon() {
-  
+    if (gold >= 30) {
+    
+    }
   }
 
   function fightSlime() {
