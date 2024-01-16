9b15fc80ba1cc3f2451bf2f94b1c869423c3271e
FRJ74 <frankr74@gmail.com>
2024-01-16 02:12:07 -0600

    Add an inner.Text & onclick element to the outer else

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ function goCave() {
   }
   
   function buyWeapon() {
-    if (currentWeapon < weapons.length)   { 
+    if (currentWeapon < weapons.length -1)   { 
         if (gold >= 30) {
         gold -= 30;
         currentWeapon ++ ;
@@ -117,7 +117,11 @@ function goCave() {
       } else {
         text.innerText="You do not have enough gold to buy a weapon.";
       }
-    }
+    } else {
+      text.innerText = "You already have the most powerful weapon!";
+      button2.innerText="Sell weapon for 15 gold";
+      button2.onclick= sellWeapon;
+  }
 }
 
   function fightSlime() {
