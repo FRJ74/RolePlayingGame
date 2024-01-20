bf379015debf07f1dca8edbdc285f10cc9901135
FRJ74 <frankr74@gmail.com>
2024-01-19 23:11:19 -0600

    Add fight property to locations array

--- script.js
+++ script.js
@@ -52,6 +52,12 @@ const locations = [
       "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
       "button functions": [fightSlime, fightBeast, goTown],
       text: "You enter the cave. You see some monsters." 
+  },
+  {
+    name: "fight",
+    "button text": ["Attack", "Dodge", "Run"],
+    "button functions": [attack, dodge, goTown],
+    text: "You are fighting a monster."
   } 
 ];
 
@@ -157,4 +163,12 @@ function sellWeapon() {
 
   function goFight() {
 
+  }
+
+  function attack() {
+
+  }
+  
+  function dodge() {
+  
   }
