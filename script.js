f71f495f3393e7953f283a7979de892325784378
FRJ74 <frankr74@gmail.com>
2024-01-28 14:00:44 -0600

    Return the value of hit

--- script.js
+++ script.js
@@ -203,6 +203,8 @@ function sellWeapon() {
 
   function getMonsterAttackValue(level) {
     const hit = (level * 5) -  (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
   }
   
   function dodge() {
